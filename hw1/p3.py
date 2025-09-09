bae2031bc7275f6efab750196a0481b9d577037b
Emily Foley <emilyfoley@Emilys-MacBook-Air-3.local>
2025-09-08 20:31:19 -0700

    fix

--- hw1/p3.py
+++ hw1/p3.py
@@ -40,10 +40,10 @@ def quadratic(a, b, c):
     disc = b**2 - 4*a*c
 
     if disc < 0:
-        return [None, None]     # no real roots
+        return [None, None]      # no real roots
     elif disc == 0:
         x = -b / (2*a)
-        return [x, x]           # repeated root
+        return [x, x]            # repeated root
     else:
         sqrt_disc = math.sqrt(disc)
         if b >= 0:
